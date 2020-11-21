b9d14890bfecf4186c51dfa28c392be7c3302647
mauricio <nmortizf@unal.edu.co>
2020-11-21 15:36:17 -0500

    Agregando solo la clase

--- HolaMundoGit/src/com/prueba/git/TestVersionamiento.java
+++ HolaMundoGit/src/com/prueba/git/TestVersionamiento.java
@@ -4,7 +4,8 @@ public class TestVersionamiento {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Mi primer archivo con Git!!!");
+		System.out.println("Mi Segundo archivo en Git");
+		System.out.println("Agregue Otro Mensaje en git");
 
 	}
 
