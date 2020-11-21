7091cd0245b48d1a833b534b56a31dd1ed0ccade
mauricio <nmortizf@unal.edu.co>
2020-11-21 16:02:33 -0500

    Pasando la clase Casa mi perro!

---
+++ HolaMundoGit/src/com/prueba/git/Casa.java
@@ -0,0 +1,17 @@
+package com.prueba.git;
+
+public class Casa {
+
+	private int numeroPersonas;
+
+	public int getNumeroPersonas() {
+		return numeroPersonas;
+	}
+
+	public void setNumeroPersonas(int numeroPersonas) {
+		this.numeroPersonas = numeroPersonas;
+	}
+	
+	
+	
+}
