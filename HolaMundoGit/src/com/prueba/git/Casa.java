b5c99917bcc9f635ba372d7f0959147da0e8f61f
mauricio <nmortizf@unal.edu.co>
2020-11-21 16:30:02 -0500

    Cambios adicionales en las clases para ver el proyecto

--- HolaMundoGit/src/com/prueba/git/Casa.java
+++ HolaMundoGit/src/com/prueba/git/Casa.java
@@ -3,6 +3,7 @@ package com.prueba.git;
 public class Casa {
 
 	private int numeroPersonas;
+	private String mensaje;
 
 	public int getNumeroPersonas() {
 		return numeroPersonas;
@@ -11,7 +12,18 @@ public class Casa {
 	public void setNumeroPersonas(int numeroPersonas) {
 		this.numeroPersonas = numeroPersonas;
 	}
-	
-	
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	@Override
+	public String toString() {
+		return "Casa [numeroPersonas=" + numeroPersonas + ", mensaje=" + mensaje + "]";
+	}	
 	
 }
--- HolaMundoGit/src/com/prueba/git/TestVersionamiento.java
+++ HolaMundoGit/src/com/prueba/git/TestVersionamiento.java
@@ -6,6 +6,11 @@ public class TestVersionamiento {
 		
 		System.out.println("Mi Segundo archivo en Git");
 		System.out.println("Agregue Otro Mensaje en git");
+		
+		Casa casita = new Casa();
+		casita.setMensaje("Una casa blanca ubicada en la plata Argentina");
+		casita.setNumeroPersonas(5);
+		System.out.println(casita);
 
 	}
 
