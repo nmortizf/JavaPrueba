99ad9297a31a7a6b7725c6025d1f2efbca724949
mauricio <nmortizf@unal.edu.co>
2020-11-29 13:01:50 -0500

    Test with user cases using a list of String

--- Domicilios/src/Logic/Test.java
+++ Domicilios/src/Logic/Test.java
@@ -1,25 +1,32 @@
 package Logic;
 
+import java.util.*;
+
 public class Test {
 
 	public static void main(String [] args) {
-		String commands = "AAAAIAA";
-		char [] letter = commands.toCharArray();
+		List<String> list = new ArrayList<>();
+		list.add("AAAAIAA");
+		list.add("DDDDAIA");
+		list.add("AADADAI");
+		char [] letter;		
 		Point p1 = new Point(0,0,'N');
 		Movements m1 = new Movements();
-		System.out.println("El punto inicial es " +  p1);
-		System.out.println();
-		for(char a : letter){
-			System.out.println(a);
-			if (a != 'A'){
-			m1.changeDirection(p1, a);
+		System.out.println("El punto inicial es " + p1.toString());
+		for(int i = 0; i < list.size(); i++) {
+			letter = list.get(i).toCharArray();
+			System.out.println();
+			for(char a : letter) {
+				if(a != 'A') {
+					m1.changeDirection(p1, a);
+				}
+				else {
+					m1.makeMovement(p1, a);
+				}
+				System.out.println("El punto " +  i + "  ahora es " + p1);
 			}
-			else {			
-			m1.makeMovement(p1, a);
-			}
-			System.out.println("El punto ahora es " +  p1);
 		}
 		System.out.println();
-		System.out.println("El punto final es " +  p1);
-	}		
+		System.out.println("El punto final es " + p1);		
+		}		
 }
